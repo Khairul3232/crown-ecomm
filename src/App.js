158a2377afc1ca916e18d95e5c4473d0a4897bcf
Kyrule A <4766351-Skyrul@users.noreply.gitlab.com>
2020-11-11 15:09:45 -0600

    Checkpoint: createUserProfile for user in to the firestore, if the user does not exist

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from './pages/shop/shop.component';
 
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth,createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() { 
@@ -19,10 +19,27 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() { 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => { 
-      this.setState({
-        currentUser: user
-      });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => { 
+      
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        // check if db has updated at that reference with the data, because we need to update state with id, and other info
+        userRef.onSnapshot(snapShot => { 
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+      else {
+        this.setState({
+          currentUser: userAuth
+        })
+      }
+
     });
   }
 
@@ -32,7 +49,6 @@ class App extends React.Component {
   }
 
   render() { 
-    console.log(this.state.currentUser);
     return (
       <div>
         <Header currentUser={ this.state.currentUser } />
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,29 @@ const config = {
     measurementId: "G-Y6K51X1ZWS"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => { 
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    // if user does not exist
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
