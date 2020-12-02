399c1f3d0c34f4a1a743fe736f02c28d413ee655
Kyrule A <4766351-Skyrul@users.noreply.gitlab.com>
2020-12-02 09:56:57 -0600

    removed the push of shop data to firebase because it was executed once already.

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ import ShopPage from './pages/shop/shop.component';
 
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth,createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
+import { auth,createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
 import CheckoutPage from './pages/checkout/checkout.component';
 
-import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
+// import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 class App extends React.Component {
 
   unsubscribeFromAuth = null;
 
   componentDidMount() { 
 
-    const { setCurrentUser, collectionsArray } = this.props;
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => { 
       
@@ -37,7 +37,7 @@ class App extends React.Component {
       }
       else {
         setCurrentUser(userAuth);
-        addCollectionAndDocuments('collections', collectionsArray.map(({ title, items }) => ({title, items})));
+        // addCollectionAndDocuments('collections', collectionsArray.map(({ title, items }) => ({title, items})));
       }
 
     });
@@ -71,8 +71,7 @@ class App extends React.Component {
 // });
 
 const mapStateToProps = createStructuredSelector ({
-  currentUser: selectCurrentUser,
-  collectionsArray: selectCollectionsForPreview
+  currentUser: selectCurrentUser
 });
 
 
