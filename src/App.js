f16e73752064aa0ffc8a21dbb0fdfc6b621c41a7
Kyrule A <4766351-Skyrul@users.noreply.gitlab.com>
2020-12-09 16:25:16 -0600

    check user session saga completed

--- src/App.js
+++ src/App.js
@@ -10,32 +10,15 @@ import { connect } from 'react-redux';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
 import CheckoutPage from './pages/checkout/checkout.component';
-
+import { checkUserSession } from './redux/user/user.actions';
 // import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 class App extends React.Component {
 
   unsubscribeFromAuth = null;
 
   componentDidMount() { 
-
-    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => { 
-      
-    //   if (userAuth) {
-    //     const userRef = await createUserProfileDocument(userAuth);
-
-    //     // check if db has updated at that reference with the data, because we need to update state with id, and other info
-    //     userRef.onSnapshot(snapShot => { 
-    //       setCurrentUser({
-    //           id: snapShot.id,
-    //           ...snapShot.data()
-    //       });
-    //     });
-    //   }
-    //   else {
-    //     setCurrentUser(userAuth);
-    //   }
-
-    // }, error => console.log("error on auth:", error));
+    const { checkUserSession } = this.props;
+    checkUserSession();
   }
 
   componentWillUnmount() { 
@@ -61,12 +44,12 @@ class App extends React.Component {
   }
 }
 
-// const mapStateToProps = ({ user }) => ({
-//   currentUser: user.currentUser
-// });
-
 const mapStateToProps = createStructuredSelector ({
   currentUser: selectCurrentUser
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => ({
+  checkUserSession: () => dispatch(checkUserSession())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
