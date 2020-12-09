19663593e46044aba7c2b0b7d272a68694011f8a
Kyrule A <4766351-Skyrul@users.noreply.gitlab.com>
2020-12-09 17:13:10 -0600

    sign out saga

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import './header.styles.scss';
-
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import { auth } from '../../firebase/firebase.utils';
-
 import { connect } from 'react-redux';
-
 import CartIcon from '../cart-icon/cart-icon.component';
-
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-
 import { createStructuredSelector } from 'reselect';
-
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { signOutStart } from '../../redux/user/user.actions';
 
-const Header = ({currentUser, hidden}) => (
+const Header = ({currentUser, hidden, signOutStart}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -31,7 +25,7 @@ const Header = ({currentUser, hidden}) => (
             </Link>
             {
                 currentUser ? (
-                    <div className='option' onClick={ ()=>auth.signOut() }>SIGN OUT</div>
+                    <div className='option' onClick={ signOutStart }>SIGN OUT</div>
                 ) : (
                     <Link className='option' to='/signin'>SIGN IN</Link>
                 )
@@ -62,4 +56,8 @@ const mapStateToProps = createStructuredSelector({
     hidden: selectCartHidden
 });
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = dispatch => ({
+    signOutStart: () => dispatch(signOutStart())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
